443449596ca6f10a6bfaf96f3e187cae6bf94e79
Timur Kazyev <50196036+kazei92@users.noreply.github.com>
2020-10-16 18:30:27 +0200

    Add files via upload

---
+++ pyobiee/classes.py
@@ -0,0 +1,73 @@
+from zeep import Client
+from zeep.transports import Transport
+from requests import Session
+
+
+class SAWSessionService():
+
+    def __init__(self, wsdl):
+        session = Session()
+        transport = Transport(session=session)
+        self.client = Client(wsdl=wsdl, transport=transport)
+        self.service = self.client.service
+
+    def logon(self, username, password):
+        session_id = self.service.logon(name=username, password=password)
+        return session_id
+
+    def logoff(self, session_id):
+        self.service.logoff(session_id)
+
+    def get_current_user(self, session_id):
+        return self.service.getCurUser(session_id)
+
+    def get_session_environment(self, session_id):
+        return self.service.getSessionEnvironment(session_id)
+
+    def get_session_variable(self, names, session_id):
+        return self.service.getSessionVariable(names, session_id)
+
+    def impersonate(self, name, password, impersonate_id):
+        session_id = self.service.getSessionVariable(name, password, impersonate_id)
+        return session_id
+    
+    def keep_alive(self, session_id):
+        self.service.keepAlive(session_id)
+    
+    def bind(self, service_name, port_name=None):
+        return self.client.bind(service_name, port_name)
+
+
+class XMLViewService():
+    def __init__(self, session_service, session_id):
+        self.service = session_service.client.bind('XmlViewService')
+        self.session_id = session_id
+        self.execution_options = {"async" : True, "maxRowsPerPage" : 10000, "refresh" : True, "presentationInfo" : True}
+    
+    def execute_sql_query(self, sql, output_format):
+        query_results = self.service.executeSQLQuery(sql=sql, outputFormat=output_format, executionOptions=self.execution_options, 
+                                                     sessionID=self.session_id)
+        return query_results
+    
+    def execute_xml_query(self, report_ref, output_format):
+        query_results = self.service.executeXMLQuery(report=report_ref, outputFormat=output_format, executionOptions=self.execution_options, 
+                                                     sessionID=self.session_id)
+        return query_results
+
+    def fetch_next(self, query_id):
+        query_results = self.service.fetchNext(queryID=query_id, sessionID=self.session_id)
+        return query_results
+
+    def cancel_query(self, query_id, session_id):
+        self.service.cancelQuery(query_id, session_id)
+
+    def get_prompted_filters(self, report_ref, session_id):
+        return self.service.getPromptedFilters(report_ref, session_id)
+
+
+def get_available_services(wsdl):
+    client = Client(wsdl=wsdl)
+    service_names = []
+    for service in client.wsdl.services.values():
+        service_names.append(service.name)
+    return service_names
---
+++ pyobiee/functions.py
@@ -0,0 +1,93 @@
+import time
+import re
+import pandas as pd
+import xml.etree.ElementTree as ET
+import logging
+from .classes import *
+logging.getLogger('zeep').setLevel(logging.ERROR)
+
+
+def get_schema(xml_view_service, query_type, path_or_sql):
+    for i in range(10):
+        if query_type == "report":
+            query_results = xml_view_service.execute_xml_query(report_ref=path_or_sql, output_format="SAWRowsetSchema")
+        elif query_type == "sql":
+            query_results = xml_view_service.execute_sql_query(sql=path_or_sql, output_format="SAWRowsetSchema")
+        else:
+            raise QueryError('unknown query type')
+
+        if query_results.rowset == None:
+            logging.getLogger("__name__").info(f'query_result is None for the attemt #{i}')
+            time.sleep(10)
+            continue
+        else:
+            break
+    
+    if query_results.rowset != None:
+        headers = parse_schema(query_results.rowset)
+        return headers
+    else:
+        raise QueryError('query returned no data, execution time is too long or there is a problem with your report')
+
+
+def parse_schema(rowset):
+    return re.findall(r'columnHeading="(.*?)"', rowset)
+
+
+def get_rows(xml_view_service, query_type, path_or_sql, headers):
+    if query_type == "report":
+        query_results = xml_view_service.execute_xml_query(report_ref=path_or_sql, output_format="SAWRowsetData")
+    elif query_type == "sql":
+        query_results = xml_view_service.execute_sql_query(sql=path_or_sql, output_format="SAWRowsetData")
+    else:
+        raise QueryError('unknown query type')
+
+    if query_results.rowset == None:
+        raise QueryError('rowset is empty, check your query')
+
+    data = parse_rows(query_results.rowset, headers)
+    while (not query_results.finished):
+        query_results = xml_view_service.fetch_next(query_id=query_results.queryID)
+        batch = parse_rows(query_results.rowset, headers)
+        data = data + batch
+    
+    return data
+
+
+def parse_rows(rowset, headers):
+    ETobject = ET.fromstring(rowset)
+    rows = ETobject.findall('{urn:schemas-microsoft-com:xml-analysis:rowset}Row')
+    data = []
+    for row in rows:
+        row_data = []
+        for i in range(len(headers)):
+            value = row.find("{urn:schemas-microsoft-com:xml-analysis:rowset}Column" + str(i))
+            if value != None:
+                row_data.append(value.text)
+            else:
+                row_data.append(None)
+        data.append(row_data)
+    
+    return data
+
+
+def get_data(query_type, path_or_sql, wsdl, username, password):
+    session_service = SAWSessionService(wsdl)
+    session_id = session_service.logon(username, password)
+    xml_view_service = XMLViewService(session_service, session_id)
+    try:
+        headers = get_schema(xml_view_service, query_type, path_or_sql)
+        data = get_rows(xml_view_service, query_type, path_or_sql, headers)
+    finally:
+        session_service.logoff(session_id)
+
+    return pd.DataFrame(data=data, columns=headers)
+
+
+class PyObieeError(Exception):
+    """PyObieeError class"""
+
+
+class QueryError(PyObieeError):
+    def __init__(self, message):
+        self.message = message
